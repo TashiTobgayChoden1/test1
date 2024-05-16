5734757a2dd70bafe3becc7f6bab73bd6d4e9c22
User <02230228.cst@rub.edu.bt>
2024-05-16 17:28:38 +0100

    .\classwork.py

---
+++ classwork.py
@@ -0,0 +1,12 @@
+name=input("Enter your name:")
+print("hello user", name)
+num1=float(input("Enter a number:"))
+num2=float(input("Enter another number:"))
+sum=num1+num2
+print("result of sum=",sum)
+difference=num1-num2
+print("result of subtraction=",difference)
+product=num1*num2
+print("result of multiplication=",product)
+quotient=num1/num2
+print("result of division",quotient)
