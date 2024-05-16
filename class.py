6b799fbba4af8eb43f47e4d06fa0b0961dfe11a8
User <02230228.cst@rub.edu.bt>
2024-05-16 17:24:55 +0100

    class

---
+++ class.py
@@ -0,0 +1,12 @@
+class Dog:
+    def __init__(self,name,color,age):
+        self.name= name
+        self.color=color
+        self.age=age
+dawa=Dog("dawa","brown",1)
+nima=Dog("nima","black",2)
+blacky=Dog("blacky","blue",3)
+
+print(dawa.name)
+print(dawa.color)
+print(dawa.age)
