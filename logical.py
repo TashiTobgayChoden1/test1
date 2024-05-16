b5f509c7bc32bb53edc93d85c79b1e8098c14c6f
User <02230228.cst@rub.edu.bt>
2024-05-16 17:35:34 +0100

    .\logical.py

---
+++ logical.py
@@ -0,0 +1,24 @@
+#  Example 1
+x=5
+print(x > 3 and x<10)
+
+# Example 2
+y=12
+print(y>10 and y%5==0)
+
+#or operator
+#Example 1
+x=5
+print(x<3 or x>10)
+
+#Example 2
+y=12
+print(y<10 or y%2==0)
+
+# not operator
+# Example 1
+x=5
+print(not(x>3 and x<10))
+# Example 2
+y=12
+print(not(y>10 and y%5==0))
